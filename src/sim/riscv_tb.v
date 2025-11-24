1731152df1c97b99c6a9161f09e8fd4e52d55b9c
Gaurav4628 <117420202+Gaurav4628@users.noreply.github.com>
2025-11-24 10:52:42 +0530

    Update riscv_tb.v

--- src/sim/riscv_tb.v
+++ src/sim/riscv_tb.v
@@ -1,1 +1,24 @@
+`timescale 1ns / 1ns
+
+module riscv_tb;
+        reg clk = 0, reset = 1;
+        riscv_core uut(clk,reset);
+        always #5 clk = ~clk;
+          initial
+            begin
+            $dumpfile("riscv.vcd");
+            $dumpvars(0, riscv_tb);
+               #3 reset = 0;
+               #95;
+
+               $display("\n---- Register values at end ----\n");
+               $display("x1 = %d", uut.u_regfile.regs[1]);
+               $display("x2 = %d", uut.u_regfile.regs[2]);
+               $display("x3 = %d", uut.u_regfile.regs[3]);
+               $display("x4 = %d", uut.u_regfile.regs[4]);
+               $display("x5 = %d", uut.u_regfile.regs[5]);
+               $display("Simulation finished");
+               $finish;
+            end
+endmodule
 
