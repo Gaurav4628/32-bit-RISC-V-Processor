642ae52d011a5b79d9ba55ffbfef60d6f82688b9
Gaurav4628 <117420202+Gaurav4628@users.noreply.github.com>
2025-11-24 10:45:45 +0530

    Update ex_mem.v

--- src/core/ex_mem.v
+++ src/core/ex_mem.v
@@ -1,1 +1,25 @@
-
+module ex_mem(
+    input clk, reset,
+    input RegWrite_in, MemtoReg_in, MemRead_in, MemWrite_in,
+    input [31:0] alu_result_in, rs2_data_in,
+    input [4:0] rd_in,
+    output reg RegWrite, MemtoReg, MemRead, MemWrite,
+    output reg [31:0] alu_result, rs2_data,
+    output reg [4:0] rd
+);
+always @(posedge clk or posedge reset) begin
+    if (reset) begin
+        RegWrite <= 0; MemtoReg <= 0;
+        MemRead <= 0; MemWrite <= 0;
+        alu_result <= 0; rs2_data <= 0; rd <= 0;
+    end else begin
+        RegWrite <= RegWrite_in;
+        MemtoReg <= MemtoReg_in;
+        MemRead <= MemRead_in;
+        MemWrite <= MemWrite_in;
+        alu_result <= alu_result_in;
+        rs2_data <= rs2_data_in;
+        rd <= rd_in;
+    end
+end
+endmodule
