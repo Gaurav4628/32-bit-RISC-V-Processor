7c328378223ac88064fa33c02cd887b5046341ff
Gaurav4628 <117420202+Gaurav4628@users.noreply.github.com>
2025-11-24 10:44:47 +0530

    Update if_id.v

--- src/core/if_id.v
+++ src/core/if_id.v
@@ -1,1 +1,21 @@
+`timescale 1ns / 100ps
+
+module if_id(input clk, reset,
+    input [31:0] pc_in,
+    input [31:0] instr_in,
+    output reg [31:0] pc_out,
+    output reg [31:0] instr_out
+);
+    always @(posedge clk or posedge reset) begin
+    if (reset) begin
+        pc_out <= 0;
+        instr_out <= 0;
+    end 
+    else begin
+        pc_out <= pc_in;
+        instr_out <= instr_in;
+    end
+    end
+
+endmodule
 
